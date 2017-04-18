3b7ed46471d1ac49a95716b001d17e25bc15c740
Zeb Burke-Conte <zmbc@uw.edu>
2017-04-18 11:47:55 -0700

    Exercises 2 & 3

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -20,74 +20,106 @@ $(function() {
     /************************************** Create chart wrappers ***************************************/
     // Create a variable `svg` by selecting the element with id `vis` and appending an svg
     // Set the width and height to your `width` and `height` variables
-
+    var svg = d3.select('#vis')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
 
     // Append a `g` element to your svg in which you'll draw your bars. Store the element in a variable called `g`, and
     // Transform the g using `margin.left` and `margin.top`
-
+    var g = svg.append('g')
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+      .attr('width', drawWidth)
+      .attr('height', drawHeight);
 
     // Load data in using d3's csv function.
     d3.csv('data/prepped_data.csv', function(error, data) {
 
         /************************************** Defining scales ***************************************/
         // Find the maximum GDP value for the maximum of the x Scale, and multiply it by 1.05 (to add space)
-
+        var maxX = d3.max(data, function(x) {return +x.gdp}) * 1.05;
 
         // Find the minimum GDP value for the minimum of the x Scale, and multiply it by .85 (to add space)
-
+        var minX = d3.min(data, function(x) {return +x.gdp}) * 0.85;
 
         // Use `d3.scaleLog` to define a variable `xScale` with the appropriate domain and range
-
+        var xScale = d3.scaleLog().domain([minX, maxX]).range([0, drawWidth]);
 
         // Find the maximum life expectance value for the maximum of the y Scale, and multiply it by 1.05 (to add space)
-
+        var maxY = d3.max(data, function(x) {return x.life_expectancy}) * 1.05;
 
         // Find the minimum life expectance value for the minimum of the y Scale, and multiply it by .9 (to add space)
-
+        var minY = d3.min(data, function(x) {return x.life_expectancy}) * 0.9;
 
         // Use `d3.scaleLinear` to define a variable `yScale` with the appropriate domain and range
-
+        var yScale = d3.scaleLinear().domain([maxY, minY]).range([0, drawHeight]);
 
         /************************************** Defining axes ***************************************/
 
         // Define x axis using d3.axisBottom, assigning the scale as the xScale
-
+        var xAxis = d3.axisBottom().scale(xScale);
 
         // Define y axis using d3.axisLeft(), assigning the scale as the yScale
-
+        var yAxis = d3.axisLeft().scale(yScale);
 
         /************************************** Rendering axes and labels ***************************************/
 
         // Append a g to your SVG as an x axis label, specifying the 'transform' attribute to position it
         // Make sure to use the `.call` method to render the axis in the label
-
+        svg.append('g')
+          .attr('transform', 'translate(' + margin.left + ', ' + (margin.top + drawHeight) + ')')
+          .call(xAxis);
 
         // Append a g to your SVG as a y axis label, specifying the 'transform' attribute to position it
         // Make sure to use the `.call` method to render the axis in the label
-
+        svg.append('g')
+          .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+          .call(yAxis);
 
         // Append a text element to your svg to label your x axis, and place it in the proper location
-
+        svg.append('text')
+          .text('GDP (log scale)')
+          .attr('fill', 'black')
+          .attr('text-anchor', 'middle')
+          .attr('transform', 'translate(' + (margin.left + (drawWidth / 2)) + ', ' + (margin.top + drawHeight + 40) + ')');
 
         // Append a text element to your svg to label your y axis, and place it in the proper location
-
+        svg.append('text')
+          .text('Life Expectancy')
+          .attr('fill', 'black')
+          .attr('text-anchor', 'middle')
+          .attr('transform', 'translate(' + (margin.left - 40) + ', ' + (margin.top + (drawHeight / 2)) + ') rotate(-90)');
 
         /************************************** Data Join ***************************************/
 
         // Select all circles and bind data
-
+        var circles = g.selectAll('circle').data(data, function(x) {return x.country_code});
 
         // Use the .enter() method to get your entering elements, and assign their positions
         // Assign a 'title' property equal to the 'country' property (for hovers)
-
+        circles.enter()
+          .append('circle')
+          .style('fill', 'blue')
+          .style('opacity', 0.5)
+          .attr('r', 10)
+          .merge(circles)
+          .attr('cx', function(x) {return xScale(+x.gdp)})
+          .attr('cy', function(x) {return yScale(+x.life_expectancy)})
+          .attr('title', function(x) {return x.country});
 
         // Use the .exit() and .remove() methods to remove elements that are no longer in the data
-
+        circles.exit()
+          .remove();
 
         /* For easy hovers, let's use jQuery to select all circles and apply a tooltip
         If you want to use bootstrap, here's a hint:
         http://stackoverflow.com/questions/14697232/how-do-i-show-a-bootstrap-tooltip-with-an-svg-object
         */
+        
+        $("circle").tooltip({
+            'container': 'body',
+            'placement': 'bottom'
+        });
 
     });
 });
--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -6,17 +6,15 @@ $(function() {
         var sex = 'female';
         var type = 'binge';
 
-        // Filter data down
-        var data = allData.filter(function(d) {
-                return d.type == type && d.sex == sex
-            })
-            // Sort the data alphabetically
-            // Hint: http://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
-            .sort(function(a, b) {
-                if (a.state_name < b.state_name) return -1;
-                if (a.state_name > b.state_name) return 1;
-                return 0;
-            });
+        $('.sex').change(function() {
+          sex = $(this).val();
+          draw();
+        });
+
+        $('.type').change(function() {
+          type = $(this).val();
+          draw();
+        });
 
         // Margin: how much space to put in the SVG for axes/titles
         var margin = {
@@ -79,71 +77,106 @@ $(function() {
         // Define a yScale with d3.scaleLinear. Domain/rage will be set in the setScales function.
         var yScale = d3.scaleLinear();
 
+        var data;
 
-        // Get the unique values of states for the domain of your x scale
-        var states = data.map(function(d) {
-            return d.state;
-        });
+        draw();
 
-        // Set the domain/range of your xScale
-        xScale.range([0, drawWidth])
-            .padding(0.1)
-            .domain(states);
+        function draw() {
+          processData();
 
-        // Get min/max values of the percent data (for your yScale domain)
-        var yMin = d3.min(data, function(d) {
-            return +d.percent;
-        });
+          updateScales();
+          updateAxes();
+          var tip = addTip();
 
-        var yMax = d3.max(data, function(d) {
-            return +d.percent;
-        });
+          // Store the data-join in a function: make sure to set the scales and update the axes in your function.
+          // Select all rects and bind data
+          var bars = g.selectAll('rect').data(data);
 
-        // Set the domain/range of your yScale
-        yScale.range([drawHeight, 0])
-            .domain([0, yMax]);
+          // Use the .enter() method to get your entering elements, and assign initial positions
+          bars.enter().append('rect')
+              .attr('x', function(d) {
+                  return xScale(d.state);
+              })
+              .attr('class', 'bar')
+              .on('mouseover', tip.show)
+              .on('mouseout', tip.hide)
+              .merge(bars)
+              .transition(500)
+              .attr('width', xScale.bandwidth())
+              .attr('y', function(d) {
+                  return yScale(d.percent);
+              })
+              .attr('height', function(d) {
+                  return drawHeight - yScale(d.percent);
+              });
+          
+          bars.exit().remove();
+        }
 
-        // Set the scale of your xAxis object
-        xAxis.scale(xScale);
-
-        // Set the scale of your yAxis object
-        yAxis.scale(yScale);
-
-        // Render (call) your xAxis in your xAxisLabel
-        xAxisLabel.call(xAxis);
-
-        // Render (call) your yAxis in your yAxisLabel
-        yAxisLabel.call(yAxis);
-
-        // Update xAxisText and yAxisText labels
-        xAxisText.text('State');
-        yAxisText.text('Percent Drinking (' + sex + ', ' + type + ')');
-
-
-        // Add tip
-        var tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
-            return d.state_name;
-        });
-        g.call(tip);
-
-        // Store the data-join in a function: make sure to set the scales and update the axes in your function.
-        // Select all rects and bind data
-        var bars = g.selectAll('rect').data(data);
-
-        // Use the .enter() method to get your entering elements, and assign initial positions
-        bars.enter().append('rect')
-            .attr('x', function(d) {
-                return xScale(d.state);
+        function processData() {
+          // Filter data down
+          data = allData.filter(function(d) {
+                return d.type == type && d.sex == sex
             })
-            .attr('class', 'bar')
-            .on('mouseover', tip.show)
-            .on('mouseout', tip.hide)
-            .attr('width', xScale.bandwidth())
-            .attr('y', function(d) {
-                return yScale(d.percent);
-            })
-            .attr('height', function(d) {
-                return drawHeight - yScale(d.percent);
+            // Sort the data alphabetically
+            // Hint: http://stackoverflow.com/questions/6712034/sort-array-by-firstname-alphabetically-in-javascript
+            .sort(function(a, b) {
+                if (a.state_name < b.state_name) return -1;
+                if (a.state_name > b.state_name) return 1;
+                return 0;
             });
+        }
+
+        function updateScales() {
+          // Get the unique values of states for the domain of your x scale
+          var states = data.map(function(d) {
+              return d.state;
+          });
+
+          // Get min/max values of the percent data (for your yScale domain)
+          var yMin = d3.min(data, function(d) {
+              return +d.percent;
+          });
+
+          var yMax = d3.max(data, function(d) {
+              return +d.percent;
+          });
+
+          // Set the domain/range of your yScale
+          yScale.range([drawHeight, 0])
+              .domain([0, yMax]);
+
+          // Set the domain/range of your xScale
+          xScale.range([0, drawWidth])
+              .padding(0.1)
+              .domain(states);
+        }
+
+        function updateAxes() {
+          // Set the scale of your xAxis object
+          xAxis.scale(xScale);
+
+          // Set the scale of your yAxis object
+          yAxis.scale(yScale);
+
+          // Render (call) your xAxis in your xAxisLabel
+          xAxisLabel.call(xAxis);
+
+          // Render (call) your yAxis in your yAxisLabel
+          yAxisLabel.call(yAxis);
+
+          // Update xAxisText and yAxisText labels
+          xAxisText.text('State');
+          yAxisText.text('Percent Drinking (' + sex + ', ' + type + ')');
+        }
+
+        function addTip() {
+          // Add tip
+          var tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
+              return d.state_name;
+          });
+          g.call(tip);
+          return tip;
+        }
     });
 });
